30cf441393c5d5e9fd5e22559a0ba604c60e74a3
Rana Universe Github CMD Prompt <RanaUniverse321@gmail.com>
2024-04-23 21:25:53 +0530

    adding the faker learning module file in the directory repo using the git cmd

---
+++ learn_faker.py
@@ -0,0 +1,30 @@
+
+
+'''
+This scripts is made by me so that i can keep tracks of 
+my faker library learning by making a git 
+### pip import faker
+Here currently i was using the Faker 24.11.0
+For any sugession Msg me in My Telegram Account:
+https://t.me/RanaUniverse 🍌🍌🍌
+
+'''
+
+
+from faker import Fake
+fake = Fake()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
